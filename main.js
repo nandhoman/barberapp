cf2e7ff58d307b44ef05b976368c26f0f00246ed
nandhoman <55098377+nandhoman@users.noreply.github.com>
2019-09-25 22:58:34 +0200

    First version

---
+++ main.js
@@ -0,0 +1,24 @@
+var herenselect = document.getElementById("herenselect");
+var herenInput = document.getElementById("heren1");
+
+var damesselect = document.getElementById("damesselect");
+var damesInput = document.getElementById("dames1");
+
+var kinderenselect = document.getElementById("kinderenselect");
+var kinderenInput = document.getElementById("kinderen1");
+    
+herenInput.addEventListener("click", () => {
+    herenselect.children[0].style.display = "block";
+});
+
+ console.log(damesselect.children[0].style.display)
+
+damesInput.addEventListener("click", () => {
+    console.log(damesselect.children)
+    damesselect.children[0].style.display = "block";
+    console.log(damesselect.children[0].style.display)
+});
+
+kinderenInput.addEventListener("click", () => {
+    kinderenselect.children[0].style.display = "block";
+});
